821910cfa4e9ff0efbf9e7443e42fb3fc1bacf3d
totekits <tote.kits@gmail.com>
2023-10-26 16:22:59 +0700

    Refactor code

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 class Mastermind
-  NUMBERS = (1..6).to_a
-
   def initialize
     @round = 1
-    @user = String.new
+    @user_is_codebreaker = false
     @secret = Array.new
     @guesses = Array.new
     @feedbacks = Array.new
+    @combos = (1..6).to_a.repeated_permutation(4).to_a
+    @combo = Array.new
+    @winner = false
   end
 
   def play
@@ -23,24 +24,25 @@ class Mastermind
       "'x' = correct number, wrong position\n" +
       "The code will be a 4-digit number ranging from 1 to 6, e.g., 1234.\n" + 
       "Good luck!\n" +
-      "Press Enter to start.."
-
+      "Enter to start.."
+    
     loop do
       puts "Welcome to Mastermind!\n" +
         "Choose your role:\n" +
         "Enter 'b' to choose Codebreaker\n" +
-        "Enter 'm' to choose Codemaker\n" +
+        "Enter 'm' to choose Codemaker\n"
+      
+      choice = gets.chomp
     
-      @user = gets.chomp.downcase
-    
-      if @user == "b"
+      if choice == "b"
         puts "You are now the Codebreaker!\n" +
           "You must guess the code within 12 rounds.\n" +
           "#{game_explanation}"
+        @user_is_codebreaker = true
         break
-      elsif @user == "m"
+      elsif choice == "m"
         puts "You are now the Codemaker!\n" +
-          "You must set a code for the Codebreaker to guess within 12 rounds." +
+          "You must set a secret code for the Codebreaker to guess within 12 rounds." +
           "#{game_explanation}"
         break
       else 
@@ -52,27 +54,35 @@ class Mastermind
   end
   
   def set_secret
-    if @user == "b"
-       @secret = NUMBERS.sample(4)
+    if @user_is_codebreaker == true
+      @secret = Array.new(4) { rand(1..6) }
+      puts "The secret code has been set!\nEnter to continue.."
+      gets
     else 
       loop do
         puts "Enter your secret code"
-        code = gets.chomp.chars.map(&:to_i)
+        secret = gets.chomp.chars.map(&:to_i)
     
-        if code.size == 4 && code.all? { |num| NUMBERS.include?(num) }
-          @secret = code
-          puts "The code has been set!\nPress Enter to start.."
+        if secret.size == 4 && secret.all? { |num| (1..6).include?(num) }
+          @secret = secret
+          puts "The secret code has been set!\nEnter to continue.."
           gets
           break
         else
-          puts "Invalid code. Please try again."
+          puts "Invalid code.\n" +
+            "Please try again with a 4-digit number using digits 1 to 6."
+        end
       end
-    end    
+    end
   end
 
   def display
     @guesses.each_with_index do |guess, index|
-      puts "Round #{index + 1}: #{@guesses[index].join} #{@feedbacks[index].join}"
+      if index < 9
+        puts "Round #{index + 1}:  #{@guesses[index].join} #{@feedbacks[index].join}"
+      else 
+        puts "Round #{index + 1}: #{@guesses[index].join} #{@feedbacks[index].join}"
+      end
     end
   end
     
@@ -96,55 +106,100 @@ class Mastermind
 
     @feedbacks << feedback
   end
-    
+
+  def find_combo
+    if @feedbacks.last.empty? == true
+      @combos.reject! { |combo| combo.include?((@guesses.last)[0])}
+    else
+      @combos.delete(@guesses.last)
+      (@feedbacks.last.size).times do
+        @combo << ((@guesses.last[0]))
+      end
+      new_guess = Array.new
+      4.times do 
+        new_guess << ((@guesses.last[0]) + 1)
+      end
+      @combos.unshift(new_guess)
+    end
+  end
+  
+  def find_positions
+    if @feedbacks.last == %w[x x x x]
+      @combos.delete(@guesses.last)
+      @combos.reject! do |combo|
+        combo.each_with_index.any? { |num, index| num == (@guesses.last)[index] }
+      end
+    elsif @feedbacks.last == %w[o x x x]
+      @combos.delete(@guesses.last)
+    elsif @feedbacks.last == %w[o o x x]
+      @combos.delete(@guesses.last)
+      new_guess = Array.new
+        new_guess << (@guesses.last)[0]
+        new_guess << (@guesses.last)[1]
+        new_guess << (@guesses.last)[3]
+        new_guess << (@guesses.last)[2]
+        @combos.unshift(new_guess)
+    else
+      @combos.reject! { |combo| combo.include?((@guesses.last)[0])}
+    end
+  end
+  
+  def check_win
+    if @user_is_codebreaker == true && @guesses.last == @secret
+        puts "You win!"
+        @winner = true
+    elsif @user_is_codebreaker == true && @round == 12 
+        puts "You lose!"
+        @winner = true
+    elsif @user_is_codebreaker == false && @guesses.last == @secret
+        puts "You lose!"
+        @winner = true
+    elsif @user_is_codebreaker == false && @round == 12
+        puts "You win!"
+        @winner = true
+    end
+  end
+
   def guess_secret
-    if @user == "b"
+    if @user_is_codebreaker == true
       loop do
-        puts "Round #{@round}: Enter your guess?"
+        puts "Round #{@round}: Enter your guess.."
         guess = gets.chomp.chars.map(&:to_i)
-        if guess.size != 4 || guess.any? { |num| !NUMBERS.include?(num) }
-          puts "Invalid guess.\n" +
-            "Please try again with a 4-digit number using digits 1 to 6."
-        else
+        if guess.size == 4 && guess.all? { |num| (1..6).include?(num) }
           @guesses << guess
           get_feedback(guess)
           display
-          if guess == @secret
-            puts "You win, you guessed the code in Round: #{@round}!"
+          check_win
+          if @winner == true
             break
-          elsif @round == 12
-            puts "You lose, the code was #{@secret.join.to_i}."
-            break
-          else
-            @round += 1
           end
+          @round += 1
+        else
+          puts "Invalid guess.\n" +
+            "Please try again with a 4-digit number using digits 1 to 6."
         end
       end
-      
-    else # code for computer to guess
-      loop do  # evaluate feedbacks and get guess
-        if @feedbacks.empty?
-          guess = Array.new(4, 1)
-        else
-          if 
-          
-        end
-        # get feedback
-        @guess << guess
+    else 
+      loop do 
+        guess = @combos.first
+        @guesses << guess
         get_feedback(guess)
+        if @combo.size < 4
+          find_combo
+        else  
+          find_positions
+        end        
         display
-        if guess == @secret
-          puts "You lose, the code was guessed in Round: #{@round}!"
+        check_win
+        if @winner == true
           break
-        elsif @round == 12
-          puts "You win, the code was never guessed!"
-          break
-        else
-          @round += 1
         end
+        @round += 1
+        puts "Enter to continue.."
+        gets
       end
     end
-  end 
+  end
 end
 
 Mastermind.new.play
